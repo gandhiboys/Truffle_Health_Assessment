32dfcb607625149f50ea8c31f3583a20c0a0a9f8
gandhiboys <manasgandhi2@gmail.com>
2023-02-13 19:49:09 -0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+//create a middleware
+var bodyParser = require('body-parser')
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+let bills = [];
+
+//endpoints
+
+//get endpoint
+app.get("/", (req, res) => {
+    // return res.json(bills);
+    res.sendFile('C://Personal Info//SDSU//Internship Docs//Truffle Health - Backend//index.html');
+});
+
+//get endpoint
+app.get("/items/get", (req, res) => {
+    return res.json(bills);
+});
+
+//creating form
+app.post("/items",urlencodedParser, (req,res) => {
+    console.log(req.body);
+    bill = req.body;
+    bills.push(bill);
+    return res.json(bill);
+    // return res.render('C://Personal Info//SDSU//Internship Docs//Truffle Health - Backend//result.html', {bills:bills});
+});
+
+
+var server = app.listen(3000, () => {
+  console.log("Server started on port 3000.");
+});
+
+module.exports = server;
---
+++ app.test.js
@@ -0,0 +1,27 @@
+const request = require("supertest");
+const app = require("./app.js");
+
+describe("Medical Bills API", () => {
+  describe("GET /items/get", () => {
+    it("should return an array of medical bills", async () => {
+      const res = await request(app).get("/items/get");
+      expect(res.statusCode).toBe(200);
+      expect(Array.isArray(res.body)).toBeTruthy();
+    });
+  });
+
+  describe("POST /items", () => {
+    it("should create a new medical bill", async () => {
+      const bill = {
+        patientName: "John Doe",
+        patientAddress: "123 Main St.",
+        hospitalName: "City Hospital",
+        dateOfService: "2022-01-01",
+        billAmount: 1000,
+      };
+      const res = await request(app).post("/items").send(bill);
+      expect(res.statusCode).toBe(200);
+      expect(res.body).toEqual(bill);
+    });
+  });
+});
